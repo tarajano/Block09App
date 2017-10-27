68a61c85e62796245a590cfc760495d65e1e385a
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-10-27 17:56:18 -0400

    SUMMARY: Defines submitMessage() to send a SMS/Text message.
    REMARKS: coursera.org/learn/android-app
    TODO:
    
    FILE: MainActivity
      ADDS: submitMessage() which uses the phone's default SMS app to send the text message entered by the user from Block09App.
    
    FILE: activity_main
      MODIFIES: The TextView where the text message is entered.

--- app/src/main/java/com/example/tarajano/block09app/MainActivity.java
+++ app/src/main/java/com/example/tarajano/block09app/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.tarajano.block09app;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsMessage;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +18,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitMessage(View view) {
+        String message = ((EditText) findViewById(R.id.multiLineText)).getText().toString();
+        Uri destination = Uri.parse("smsto:+16138697023");
+        Intent smsIntent = new Intent(Intent.ACTION_SENDTO, destination);
+        smsIntent.putExtra("sms_body", message);
+        startActivity(smsIntent);
     }
 }
